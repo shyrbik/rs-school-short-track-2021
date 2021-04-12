4bb8d3b9172c9f634db5dd410c2ab336fe5ca99b
shyrbik <58969166+shyrbik@users.noreply.github.com>
2021-04-12 11:40:56 +0300

    add: solve

--- src/01-matrix-elements-sum.js
+++ src/01-matrix-elements-sum.js
@@ -1,8 +1,26 @@
-function getMatrixElementsSum(matrix)  {
+/**
+ * Given matrix, a rectangular matrix of integers,
+ * just add up all the values that don't appear below a "0".
+ *
+ * @param {Array<Array>} matrix
+ * @return {Number}
+ *
+ * @example
+ * matrix = [
+ *  [0, 1, 1, 2],
+ *  [0, 5, 0, 0],
+ *  [2, 0, 3, 3]
+ * ]
+ *
+ * The result should be 9
+ */
+function getMatrixElementsSum(matrix) {
+  const rows = matrix.length;
+  const cols = matrix[0].length;
   let sum = 0;
 
-  for (let j = 0; j < matrix[0].length; j++) {
-    for (let i = 0; i < matrix.length;; i++) {
+  for (let j = 0; j < cols; j++) {
+    for (let i = 0; i < rows; i++) {
       if (matrix[i][j] === 0) {
         break;
       }
@@ -12,4 +30,5 @@ function getMatrixElementsSum(matrix)  {
 
   return sum;
 }
+
 module.exports = getMatrixElementsSum;
