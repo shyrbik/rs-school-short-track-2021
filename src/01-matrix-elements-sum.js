768d2ad02cf08cc612e244010fe67778d3d4cf46
shyrbik <58969166+shyrbik@users.noreply.github.com>
2021-04-12 11:35:04 +0300

    Update 01-matrix-elements-sum.js

--- src/01-matrix-elements-sum.js
+++ src/01-matrix-elements-sum.js
@@ -1,13 +1,15 @@
-function getMatrixElementsSum(matrix) {
-let sum = 0;
+function getMatrixElementsSum(matrix)  {
+  let sum = 0;
+
   for (let j = 0; j < matrix[0].length; j++) {
-    for (let i = 0; i < matrix.length; i++) {
+    for (let i = 0; i < matrix.length;; i++) {
       if (matrix[i][j] === 0) {
         break;
       }
       sum += matrix[i][j];
     }
   }
+
   return sum;
 }
 module.exports = getMatrixElementsSum;
